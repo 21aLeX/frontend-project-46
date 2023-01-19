23ff762515ff1691c17a0f9b961808cd5ec43f9c
alex <aleksandra12mail@mail.ru>
2023-01-19 23:30:54 +0800

    second commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,12 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.0');
+program
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
+    .argument('filepath1')
+    .argument('filepath2');
+
 program.parse();
 
 
