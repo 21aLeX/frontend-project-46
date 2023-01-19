356eb35caefefa6e84d3fbba8decd30eb6624e8a
alex <aleksandra12mail@mail.ru>
2023-01-19 21:24:30 +0800

    first commit

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { program } from '../node_modules/commander/esm.mjs';
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.0');
+program.parse();
+
+
