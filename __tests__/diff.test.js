a7f1e263980ec6806965a5c3d401795147987e9f
alex <aleksandra12mail@mail.ru>
2023-01-23 23:27:31 +0800

    test commit

---
+++ __tests__/diff.test.js
@@ -0,0 +1,15 @@
+import { genDiff } from "../src/index.js";
+
+const result =
+  "{\n\
+ - follow: false\n\
+   host: hexlet.io\n\
+ - proxy: 123.234.53.22\n\
+ - timeout: 50\n\
+ + timeout: 20\n\
+ + verbose: true\n\
+}";
+test("reverse", () => {
+  expect(genDiff("../file1.json", "../file2.json")).toEqual(result);
+  // expect(genDiff()).toEqual("Error, these are not json files");
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from "../node_modules/commander/esm.mjs";
-import { genDiff } from "../index.js";
+import { genDiff } from "../src/index.js";
 
 program
   .name("gendiff")
