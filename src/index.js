fc723f0181723f05ad0b80f3990aee652369083d
alex <aleksandra12mail@mail.ru>
2023-02-01 17:01:01 +0800

    new eighth commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import parse from "./parse.js";
 import formatters from "./formatters/index.js";
 import getDifference from "./getDifference.js";
 
+// так же код коверэйдж говорит о техническом долге в два дня, указывая на сортировку (видимо из лодаш)
+// и на то что функции длинее 25 строк, хотя строки выравнивает автоматом преттир
 export function genDiff(route1, route2, nameFormatter) {
   const [tree1, tree2] = parse(route1, route2);
   const data = getDifference(tree1, tree2);
