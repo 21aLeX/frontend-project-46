ac6e2f8c6002234589015da0ae7b442ea81a4e04
alex <aleksandra12mail@mail.ru>
2023-02-02 00:20:44 +0800

    new eighth commit№30

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ const getContent = (route) => {
   const fileContent = readFileSync(way, 'utf8');
   return fileContent;
 };
-// так же код коверэйдж говорит о техническом долге в два дня,
-// указывая на сортировку (видимо из лодаш)
-// и на то что функции длинее 25 строк, хотя строки выравнивает автоматом преттир
+// почему линтер не показывает ошибки, и не исправляет их?
 export default (route1, route2, nameFormatter) => {
   const obj = [
     [getContent(route1), path.extname(route1)],
-    [getContent(route2), path.extname(route2)]
+    [getContent(route2), path.extname(route2)],
   ];
   const [tree1, tree2] = parse(obj);
   const data = getDifference(tree1, tree2);
