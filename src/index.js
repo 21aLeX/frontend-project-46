0a8f4d696841eb35a1271521ef460731793ac90c
alex <aleksandra12mail@mail.ru>
2023-02-02 00:13:54 +0800

    new eighth commit№28

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+import { readFileSync } from 'fs';
+import path from 'path';
 import parse from './parse.js';
 import formatters from './formatters/index.js';
 import getDifference from './getDifference.js';
 
+const getContent = (route) => {
+  const way = path.resolve(route);
+  const fileContent = readFileSync(way, 'utf8');
+  return fileContent;
+};
+
+
 // так же код коверэйдж говорит о техническом долге в два дня,
 // указывая на сортировку (видимо из лодаш)
 // и на то что функции длинее 25 строк, хотя строки выравнивает автоматом преттир
-export default (route, nameFormatter) => {
-  const [tree1, tree2] = parse(route);
+export default (route1, route2, nameFormatter) => {
+  const obj = [[getContent(route1), path.extname(route1)], [getContent(route2), path.extname(route2)]];
+  const [tree1, tree2] = parse(obj);
   const data = getDifference(tree1, tree2);
   return formatters(data, nameFormatter);
 };
--- src/parse.js
+++ src/parse.js
@@ -1,21 +1,12 @@
-import path from 'path';
 import yaml from 'js-yaml';
-import { readFileSync } from 'fs';
 
-const getContent = (route) => {
-  const way = path.resolve(route);
-  const fileContent = readFileSync(way, 'utf8');
-  return fileContent;
-};
-export default (route) => route.map((item) => {
-  const exstension = path.extname(item);
+export default (data) => data.map(([obj, exstension]) => {
   switch (exstension) {
-    // то что по дефолту ошибку пробрасывать, тоже не сама догадалась(
     case '.json':
-      return JSON.parse(getContent(item));
+      return JSON.parse(obj);
     case '.yml':
     case '.yaml':
-      return yaml.load(getContent(item));
+      return yaml.load(obj);
     default:
       throw new Error(`I don't know how to deal with files with extension ${exstension}`);
   }
