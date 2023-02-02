18ccb9d4b22d61dcde94b649d72d35461ba37807
alex <aleksandra12mail@mail.ru>
2023-02-02 20:24:48 +0800

    new eighth commit№41

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -10,39 +10,25 @@ const signs = {
 const getIndent = (depth) => replacer.repeat(depth * spacesCount - 2);
 const stylish = (value) => {
   const iter = (currentValue, depth) => {
-    if (!_.isObject(currentValue)) {
-      return currentValue;
-    }
+    if (!_.isObject(currentValue)) { return currentValue; }
     const lines = Object.entries(currentValue).map(([key, val]) => {
       const indent = getIndent(depth);
       switch (val.type) {
         case 'added':
         case 'deleted':
         case 'unchanged':
-          return `${indent}${signs[val.type]}${key}: ${iter(
-            val.value,
-            depth + 1,
-          )}`;
+          return `${indent}${signs[val.type]}${key}: ${iter(val.value, depth + 1)}`;
         case 'nested':
           return `${indent}  ${key}: ${iter(val.children, depth + 1)}`;
         case 'changed':
-          return `${indent}- ${key}: ${iter(
-            val.value1,
-            depth + 1,
-          )}\n${indent}+ ${key}: ${iter(val.value2, depth + 1)}`;
+          return `${indent}- ${key}: ${iter(val.value1, depth + 1)}\n${indent}+ ${key}: ${iter(val.value2, depth + 1)}`;
         default:
           return `${indent}  ${key}: ${iter(val, depth + 1)}`;
       }
     });
-
-    return [
-      '{',
-      ...lines,
-      `${replacer.repeat((depth - 1) * spacesCount)}}`,
-    ].join('\n');
+    return ['{', ...lines,
+      `${replacer.repeat((depth - 1) * spacesCount)}}`].join('\n');
   };
-
   return iter(value, 1);
 };
-
 export default stylish;
--- src/getDifference.js
+++ src/getDifference.js
@@ -3,23 +3,22 @@ import _ from 'lodash';
 const getDifference = (obj1, obj2) => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const result = keys.reduce((acc, key) => {
+    let newItem;
     if (!_.has(obj1, key)) {
-      return { ...acc, [key]: { type: 'added', value: obj2[key] } };
-    }
-    if (!_.has(obj2, key)) {
-      return { ...acc, [key]: { type: 'deleted', value: obj1[key] } };
-    }
-    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return {
-        ...acc,
+      newItem = { [key]: { type: 'added', value: obj2[key] } };
+    } else if (!_.has(obj2, key)) {
+      newItem = { [key]: { type: 'deleted', value: obj1[key] } };
+    } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      newItem = {
         [key]:
           { type: 'nested', children: getDifference(obj1[key], obj2[key]) },
       };
+    } else if (!_.isEqual(obj1[key], obj2[key])) {
+      newItem = { [key]: { type: 'changed', value1: obj1[key], value2: obj2[key] } };
+    } else {
+      newItem = { [key]: { type: 'unchanged', value: obj1[key] } };
     }
-    if (!_.isEqual(obj1[key], obj2[key])) {
-      return { ...acc, [key]: { type: 'changed', value1: obj1[key], value2: obj2[key] } };
-    }
-    return { ...acc, [key]: { type: 'unchanged', value: obj1[key] } };
+    return { ...acc, ...newItem };
   }, {});
   // почему нужно получить именно массив с объектами разной вложенности?
   // почему для этого не подойдет обьект?
