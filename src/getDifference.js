2b5fb45e3285fd5e70be576eb803cc9d8e534afb
alex <aleksandra12mail@mail.ru>
2023-02-02 20:42:12 +0800

    new eighth commit№43

--- src/getDifference.js
+++ src/getDifference.js
@@ -3,20 +3,17 @@ import _ from 'lodash';
 const getDifference = (obj1, obj2) => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const result = keys.reduce((acc, key) => {
-    let newItem;
+    const newItem = {};
     if (!_.has(obj1, key)) {
-      newItem = { [key]: { type: 'added', value: obj2[key] } };
+      newItem[key] = { type: 'added', value: obj2[key] };
     } else if (!_.has(obj2, key)) {
-      newItem = { [key]: { type: 'deleted', value: obj1[key] } };
+      newItem[key] = { type: 'deleted', value: obj1[key] };
     } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      newItem = {
-        [key]:
-          { type: 'nested', children: getDifference(obj1[key], obj2[key]) },
-      };
+      newItem[key] = { type: 'nested', children: getDifference(obj1[key], obj2[key]) };
     } else if (!_.isEqual(obj1[key], obj2[key])) {
-      newItem = { [key]: { type: 'changed', value1: obj1[key], value2: obj2[key] } };
+      newItem[key] = { type: 'changed', value1: obj1[key], value2: obj2[key] };
     } else {
-      newItem = { [key]: { type: 'unchanged', value: obj1[key] } };
+      newItem[key] = { type: 'unchanged', value: obj1[key] };
     }
     return { ...acc, ...newItem };
   }, {});
